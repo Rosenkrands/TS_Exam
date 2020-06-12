fcdd243513faf65f4b2cd8a84f5f28d251248350
Rosenkrands <KRosenkrands7@gmail.com>
2020-06-12 20:08:16 +0200

    clean up file structure and add header to all

--- misc/set-content.ps1
+++ misc/set-content.ps1
@@ -1,1 +1,10 @@
-﻿cd 'C:\Users\Kasper\Desktop\github_repos\TS_Exam'
+﻿1..8 | ForEach-Object {New-Item -type file -path C:\time-series-exam\$_.Rmd}
+
+ $header = get-content -path $PSScriptRoot\yaml_header.txt
+
+$files = Get-ChildItem -path C:\time-series-exam\*.Rmd
+
+foreach ($file in $files) {
+    Set-Content -path $file -Value $header
+    write-host "$file - success"
+}
